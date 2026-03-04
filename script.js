03335e5a0cb49bb1161938db90c24929dea1b638
yasir <mohammadyasireqbal@gmail.com>
2026-03-05 00:18:12 +0530

    handled errors for fetch

--- script.js
+++ script.js
@@ -18,20 +18,30 @@ searchBar.addEventListener("keydown", (event) => {
 
 //fuction to get profile data using username
 async function getProfile(username){
-    let rawProfile= await fetch(`https://api.github.com/users/${username}`)
-    let profile= await rawProfile.json()
-    let profileData= {avatar_url:profile.avatar_url, name:profile.name, username: profile.login, bio:profile.bio, followerCount:profile.followers, followingCount:profile.following, repoCount:profile.public_repos, url:profile.html_url}
-    return profileData
+    try{
+        let rawProfile= await fetch(`https://api.github.com/users/${username}`)
+        let profile= await rawProfile.json()
+        let profileData= {avatar_url:profile.avatar_url, name:profile.name, username: profile.login, bio:profile.bio, followerCount:profile.followers, followingCount:profile.following, repoCount:profile.public_repos, url:profile.html_url}
+        return profileData
+    }
+    catch(err){
+        console.log(err.message)
+    }
 }
 
 // function to get top 5 repos using username
 async function getRepos(username){
-    let rawRepos= await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=5`)
-    let repos= await rawRepos.json()
-    let reposData= repos.map((repo)=>{
-        return {name: repo.name, description: repo.description, language: repo.language, starCount:repo.stargazers_count, forkCount: repo.forks_count, repoLink: repo.html_url}
-    })
-    return reposData
+    try{
+        let rawRepos= await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=5`)
+        let repos= await rawRepos.json()
+        let reposData= repos.map((repo)=>{
+            return {name: repo.name, description: repo.description, language: repo.language, starCount:repo.stargazers_count, forkCount: repo.forks_count, repoLink: repo.html_url}
+        })
+        return reposData
+    }
+    catch(err){
+        console.log(err.message)
+    }
 }
 
 // Call the functions to get Profile Data and Repos Data and call render function
