717721d29a2eec3437b97af307d9cf62672e3453
yasir <mohammadyasireqbal@gmail.com>
2026-03-04 23:12:28 +0530

    added js for rendering profile and repos

--- script.js
+++ script.js
@@ -34,12 +34,92 @@ async function getRepos(username){
     return reposData
 }
 
-// Call the functions to get Profile Data and Repos Data
+// Call the functions to get Profile Data and Repos Data and call render function
 async function getData(){
     let username= getUsername()
+
     let profileData= await getProfile(username)
+    renderProfile(profileData)
     console.log(profileData) 
+
     let reposData= await getRepos(username)
+    renderRepos(reposData)
     console.log(reposData)
 }
 
+
+// render functions
+
+// render function for profile card
+let card = document.querySelector(".card")
+function renderProfile(profileData){
+    if (profileData.bio===null){
+        profileData.bio=""
+    }
+    card.innerHTML= `<div class="card-header"></div>
+            
+            <div class="card-body">
+                <div class="avatar-wrapper">
+                    <img src="${profileData.avatar_url}" alt="${profileData.name}" class="avatar">
+                </div>
+
+                <h1 class="name">${profileData.name}</h1>
+                <p class="handle">${profileData.username}</p>
+                <p class="bio">${profileData.bio}</p>
+
+                <hr class="divider">
+
+                <div class="stats">
+                    <div class="stat-item">
+                        <i class="fa-solid fa-people-line stat-icon"></i>
+                        <span class="stat-value">${profileData.followerCount}</span>
+                        <span class="stat-label">Followers</span>
+                    </div>
+                    <div class="stat-item">
+                        <i class="fa-solid fa-user stat-icon"></i>
+                        <span class="stat-value">${profileData.followingCount}</span>
+                        <span class="stat-label">Following</span>
+                    </div>
+                    <div class="stat-item">
+                        <i class="fa-solid fa-folder-open stat-icon"></i>
+                        <span class="stat-value">${profileData.repoCount}</span>
+                        <span class="stat-label">Public Repos</span>
+                    </div>
+                </div>
+
+                <a href="${profileData.url}" target="_blank" class="btn-profile">View on GitHub</a>
+            </div>`
+}
+
+// render function for repo cards
+let repoContainer= document.querySelector(".repo-container")
+function renderRepos(reposData){
+    let reposHTML= `<h2 class="section-title">Top 5 Recently Updated Repositories</h2>`
+    reposData.forEach((elem)=>{
+        reposHTML+=`<a href="${elem.repoLink}">
+                <div class="repo-list">
+                    
+                    <div class="repo-card">
+                        <div class="repo-info">
+                            <h3 class="repo-name">${elem.name}</h3>
+                            <p class="repo-desc">${elem.description}</p>
+                            <div class="repo-meta">
+                                <span class="lang-dot" style="background-color: #e34c26;"></span>
+                                <span class="lang-name">${elem.language}</span>
+                            </div>
+                        </div>
+                        <div class="repo-stats">
+                            <div class="stat-col">
+                                <i class="fa-solid fa-star stat-icon"></i>
+                                <span class="stat-value">${elem.starCount}</span>
+                            </div>
+                            <div class="stat-col">
+                                <i class="fa-solid fa-code-fork stat-icon"></i>
+                                <span class="stat-value">${elem.forkCount}</span>
+                            </div>
+                        </div>
+                    </div>
+            </a>`
+    })
+    repoContainer.innerHTML=reposHTML
+}
